8ff10abfef3bb62305f5cd3234034fd757b03811
vikram <vikramdev772@gmail.com>
2023-05-25 11:14:13 +0530

    React-Basics

---
+++ MyTestApp/App.tsx
@@ -0,0 +1,33 @@
+
+import React from "react";
+
+import {
+
+  View,
+  Text,
+  SafeAreaView
+
+
+
+} from 'react-native'
+
+
+function App(){
+  
+  return (
+    <SafeAreaView>
+      <Text>A React Native component that allows you to input text and formats the text 
+        into a tag when a space or comma is entered. </Text>
+
+
+
+
+    </SafeAreaView>
+  )
+
+
+}
+
+
+
+export default App;
